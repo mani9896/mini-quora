6fc171b9f367ec7db0037afd951dac9f5678c194
mani <54025138+mani9896@users.noreply.github.com>
2020-10-01 02:01:18 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to mini");
+});
+
+app.listen(3000 || PORT, function (req, res) {
+  console.log("Running ");
+});
