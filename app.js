4fb04f6cf5de1b7f4e1c281ff4972eda1ec8ab5e
mani9896 <shankar.money08@gmail.com>
2020-10-02 17:25:03 +0530

    made routes folder

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ const db = require("./config/db.js");
 const PORT = process.env.PORT;
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-
+app.use(express.json({ extended: false }));
 const name = "Mani";
 
 //render home.ejs with passing a variable
 app.get("/", (req, res) => {
   res.render("Home", { name: name });
 });
+app.use("/user", require("./routes/user"));
 app.listen(3000 || PORT, function (req, res) {
   console.log("Running on Server");
 });
---
+++ routes/user.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+const db = require("../config/db.js");
+const PORT = process.env.PORT;
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+const router = express.Router();
+
+router.get("/login", (req, res) => {
+  res.render("Home", { name: "hjhgh" });
+});
+
+module.exports = router;
