417eab277e1e4e67726558b580917153bfdec174
mani9896 <shankar.money08@gmail.com>
2020-10-03 22:39:02 +0530

    Added unique username to singup

--- routes/middleware/registerMiddleware.js
+++ routes/middleware/registerMiddleware.js
@@ -1,18 +1,31 @@
 const bodyParser = require("body-parser");
 const db = require("../../config/db.js");
 
-const checkIfUserExists = (req, res, next) =>
-  db.query("SELECT * FROM user WHERE email = ?", req.body.Email, function (
-    error,
-    results,
-    fields
-  ) {
-    if (results.length > 0) {
-      res.render("SignUP", { msg: "USER ALREADY REGISTERED" });
-      ifUserfound = true;
-    } else {
-      next();
+const checkIfUserExists = async (req, res, next) => {
+  var msg = [];
+  await db.query(
+    "SELECT * FROM user WHERE email = ?",
+    req.body.Email,
+    function (error, results, fields) {
+      if (results.length > 0) {
+        msg.push("USERA LREADY");
+        ifUserfound = true;
+      }
+      db.query("SELECT * FROM user WHERE email = ?", req.body.Email, function (
+        error,
+        results,
+        fields
+      ) {
+        if (results.length > 0) {
+          msg.push("ALREADY HERE");
+          res.render("SignUP", { msg: msg });
+          ifUserfound = true;
+        } else {
+          next();
+        }
+      });
     }
-  });
+  );
+};
 
 module.exports = checkIfUserExists;
