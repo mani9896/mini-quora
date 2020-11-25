24df8bbaf87d7cc96109919610d27bfd6235c102
mani9896 <shankar.money08@gmail.com>
2020-11-25 14:25:13 +0530

    trying

--- app.js
+++ app.js
@@ -30,11 +30,47 @@ app.get("/", async (req, res) => {
     msg = ["Logged In"];
   }
   await db.query(
-    "SELECT title,text,url FROM POST,IMAGES WHERE POST.post_id=IMAGES.post_id",
+    "SELECT POST.post_id,title,text,url FROM POST,IMAGES WHERE POST.post_id=IMAGES.post_id",
     function (error, result, fileds) {
       if (error) {
         console.log(error);
       } else {
+        var post = {
+          post_id: "",
+          title: "",
+          text: "",
+          url: [],
+        };
+        var posts = [];
+        var j = 0;
+        var i = j;
+
+        for (i = j; i < result.length; i++) {
+          post = {
+            post_id: result[i].post_id,
+            title: result[i].title,
+            text: result[i].text,
+            urls: [result[i].url],
+          };
+          console.log(post);
+
+          for (j = i + 1; j < result.length; j++) {
+            console.log(post + " hbh " + result[j]);
+            if (post.post_id === result[j].post_id) {
+              console.log(post.urls);
+              post.urls.push(result[j].url);
+              console.log("RUnning");
+              console.log(post.urls);
+            } else {
+              posts.push(post);
+              console.log("BREAKING");
+              i = j;
+              break;
+            }
+          }
+        }
+        // console.log(posts);
+
         res.render("Home", {
           posts: result,
           msg: msg,
