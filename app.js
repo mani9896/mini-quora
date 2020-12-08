0f97e67759980bacd008789e5c77a1028f18d0e7
mani9896 <shankar.money08@gmail.com>
2020-12-08 15:38:08 +0530

    Done

--- app.js
+++ app.js
@@ -30,47 +30,11 @@ app.get("/", async (req, res) => {
     msg = ["Logged In"];
   }
   await db.query(
-    "SELECT POST.post_id,title,text,url FROM POST,IMAGES WHERE POST.post_id=IMAGES.post_id",
+    "SELECT images.post_id, title, text, ANY_VALUE(url) AS url FROM post,IMAGES where post.post_id = images.post_id group by post_id",
     function (error, result, fileds) {
       if (error) {
         console.log(error);
       } else {
-        var post = {
-          post_id: "",
-          title: "",
-          text: "",
-          url: [],
-        };
-        var posts = [];
-        var j = 0;
-        var i = j;
-
-        for (i = j; i < result.length; i++) {
-          post = {
-            post_id: result[i].post_id,
-            title: result[i].title,
-            text: result[i].text,
-            urls: [result[i].url],
-          };
-          console.log(post);
-
-          for (j = i + 1; j < result.length; j++) {
-            console.log(post + " hbh " + result[j]);
-            if (post.post_id === result[j].post_id) {
-              console.log(post.urls);
-              post.urls.push(result[j].url);
-              console.log("RUnning");
-              console.log(post.urls);
-            } else {
-              posts.push(post);
-              console.log("BREAKING");
-              i = j;
-              break;
-            }
-          }
-        }
-        // console.log(posts);
-
         res.render("Home", {
           posts: result,
           msg: msg,
